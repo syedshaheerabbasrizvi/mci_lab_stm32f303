86fbf84b3eade30ff7ac9d473b4357752577f86b
Shaheer Abbas <114511098+syedshaheerabbasrizvi@users.noreply.github.com>
2024-10-01 00:22:45 +0500

    Delete Templates_LL directory

--- Templates_LL/MDK-ARM/STM32F3-Discovery/stm32f3xx_ll_dac.d
+++
@@ -1,8 +0,0 @@
-stm32f3-discovery/stm32f3xx_ll_dac.o: \
-  ..\..\..\..\Drivers\STM32F3xx_HAL_Driver\Src\stm32f3xx_ll_dac.c \
-  ..\..\..\..\Drivers\STM32F3xx_HAL_Driver\Inc\stm32f3xx_ll_dac.h \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f3xx.h \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f303xc.h \
-  D:\MDK-ARM\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\system_stm32f3xx.h \
-  ..\..\..\..\Drivers\STM32F3xx_HAL_Driver\Inc\stm32f3xx_ll_bus.h
--- Templates_LL/MDK-ARM/STM32F3-Discovery/system_stm32f3xx.d
+++
@@ -1,5 +0,0 @@
-stm32f3-discovery/system_stm32f3xx.o: ..\Src\system_stm32f3xx.c \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f3xx.h \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f303xc.h \
-  D:\MDK-ARM\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
-  ..\..\..\..\Drivers\CMSIS\Device\ST\STM32F3xx\Include\system_stm32f3xx.h
